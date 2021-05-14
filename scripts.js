a30ab4e9cf7a18d106713d8ff661e9da67764198
ukasz1 <46789003+ukasz1@users.noreply.github.com>
2021-05-14 15:17:29 +0200

    checking errors added, form validation required

--- scripts.js
+++ scripts.js
@@ -33,7 +33,28 @@ function czas(){
 	setTimeout("czas()",1000);
 }
 //--------------------------------------------------------------------------------------- error flags
-let dayErr=true, monthErr=true, yearErr=true, hourErr=true, minuteErr=true;
+let eventErr=true, dayErr=true, monthErr=true, yearErr=true, hourErr=true, minuteErr=true;
+
+/*
+	eventErr=true 	-	the event description is empty
+	dayErr=true		-	day is incorrect
+	monthErr=true	-	month is incorrect
+	yearErr=true 	-	year is incorrect
+	hourErr=true	-	hour is incorrect
+	minuteErr=true	-	minute is incorrect
+*/
+
+//---------------------------------------------------------------------------------------
+let description = document.getElementById('description');
+
+function descriptionCheck(){
+	if(description.value == '')
+		eventErr=true;
+	else
+		eventErr=false;
+}
+
+description.addEventListener('change', descriptionCheck, false);
 
 //---------------------------------------------------------------------------------------
 let dayId=document.getElementById('dayId');
@@ -91,3 +112,51 @@ function yearCheck(){
 }
 
 yearId.addEventListener('blur', yearCheck, false);
+
+//---------------------------------------------------------------------------------------
+let hourId = document.getElementById('hour-Id');
+
+const hourCheck = () => {
+	if(hourId.value == '')	//here add a comment, add validation
+		hourErr = true;
+	else
+		hourErr = false;
+}
+
+hourId.addEventListener('blur', hourCheck, false);
+
+//---------------------------------------------------------------------------------------
+let minuteId = document.getElementById('minute-Id');
+
+const minuteCheck = () => {
+	if(minuteId.value == '')
+		minuteErr = true;
+	else
+		minuteErr = false;
+}
+
+minuteId.addEventListener('blur', minuteCheck, false);
+
+//---------------------------------------------------------------------------------------
+
+const btn = document.getElementById('submit-form');
+
+btn.addEventListener('click', e => {
+	
+	e.preventDefault();
+	
+	if(eventErr + dayErr + monthErr + yearErr + hourErr + minuteErr !=0)
+		console.log('Błędny formularz');
+	else
+		console.log('Wszystko git!');
+	
+	console.log(`
+	eventErr: ${eventErr}
+	dayErr: ${dayErr}
+	monthErr: ${monthErr}
+	yearErr: ${yearErr}
+	hourErr: ${hourErr}
+	minuteErr: ${minuteErr}
+`);
+	
+}, false);
