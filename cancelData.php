7820025fa1eee2c275d7349a0af72a958e06fda6
ukasz1 <46789003+ukasz1@users.noreply.github.com>
2021-12-10 11:49:11 +0100

    added functionality of cancelling an item
    cancelling item PHP scripts added

--- cancelData.php
+++ cancelData.php
@@ -1,10 +1,27 @@
 <?php
 
 if(isset($_POST['input_1'])){  
-    $dane = $_POST['input_1'];
-    echo $dane;
+
+    require_once "connect.php";
+
+    $polaczenie = new mysqli($host, $db_user, $db_password, $db_name); //połączenie z DB
+
+    if ($polaczenie->connect_errno!=0){     // czy jest błąd połączenia
+        echo "Error: ".$polaczenie->connect_errno;
+    }
+    else{
+        $dane = (int) $_POST['input_1'];
+        $sql = "DELETE FROM deadline WHERE id=$dane";
+
+        $rezultat = @$polaczenie->query($sql);
+
+        $polaczenie->close();
+        
+        header('Location: index.html');
+    }
+
 }
 else
     header('Location: index.html');
-    
+
 ?>
