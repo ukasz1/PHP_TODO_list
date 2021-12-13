48c0d6df7bf23cc7ee61c6c5451f1756697549ed
ukasz1 <46789003+ukasz1@users.noreply.github.com>
2021-12-13 11:40:25 +0100

    SQL injection preventing
    htmlentities() function added

--- newRecord.php
+++ newRecord.php
@@ -23,13 +23,14 @@
         $hour = $_POST['hour'];
         $minute = $_POST['minute'];
 
-        //$nextWeek = time() + (7 * 24 * 60 * 60)
+        $description = htmlentities($description, ENT_QUOTES, "UTF-8");
+        
         $date=date_create("$year-$month-$day");
         date_time_set($date, $hour, $minute);
 
         $unixTime = date_timestamp_get($date);
         //$unixTime = $unixTime*1000;
-
+/*
         echo $description."<br />";
         echo $taskName."<br />";
         echo $day."<br />";
@@ -38,42 +39,18 @@
         echo $hour."<br />";
         echo $minute."<br />";
         echo $unixTime."<br />";
-
+*/
         $sql = "INSERT INTO deadline VALUES (NULL, '$taskName', '$description', $day, $month, $year, $hour, $minute, $unixTime)";
-
-        /*(NULL, $taskName, $description, $day, $month, $year, $hour, $minute, $unixTime)"; */
 
         if($rezultat = $polaczenie->query($sql)){
             echo "Wpisano do bazy";
         }
         else{
-            echo "Coś zwalone <br />";
             echo "Error: ";
         }
 
         $polaczenie->close();
         header('Location: index.html');
-
-        //header('Location: index.html');
     }
-    
-/*
-<select name="month" id="monthId">
-						<option value="month">Miesiąc</option>
-						<option value="junuary">styczeń</option>
-						<option value="february">luty</option>
-						<option value="march">marzec</option>
-						<option value="april">kwiecień</option>
-						<option value="may">maj</option>
-						<option value="june">czerwiec</option>
-						<option value="july">lipiec</option>
-						<option value="august">sierpień</option>
-						<option value="september">wrzesień</option>
-						<option value="october">październik</option>
-						<option value="november">listopad</option>
-						<option value="december">grudzień</option>
-
-*/
-
 ?>
 
